991c4fba12a6f348463a624404ac439513ecdbcb
Edilaine Alves dos Santos <edilaine.santos839@gmail.com>
2022-02-10 19:18:08 -0300

    animacao-carousel

---
+++ portifolio-pessoal/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#btn-prev').click(function() {
+        $('#carouselExampleIndicators').carousel('prev');
+      });
+      
+      $('#btn-next').click(function() {
+        $('#carouselExampleIndicators').carousel('next');
+      });
+});
